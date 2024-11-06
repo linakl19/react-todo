69ce34059a46ceed279570f48ba284bb2ea7f398
Lina Castro <112339197+linakl19@users.noreply.github.com>
2024-11-05 20:57:41 -0800

    Added array with 3 objects and JS expression.

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,31 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const todoList = [
+  {
+    id:1,
+    title: "Take dogs for a walk."
+  },
+  {
+    id:1,
+    title: "Make dinner."
+  },
+  {
+    id:1,
+    title: "Complete CTD assigment."
+  }
+];
 
+function App() {
   return (
     <>
       <div>
         <h1>Todo List</h1>
-        <ul></ul>
+        <ul>
+          {todoList.map(function(item){
+            return <li key={item.id}>{item.title}</li>;
+          })}
+        </ul>
       </div>
     </>
   )
