0f551292c268ef5a23066d005197a6374fe63c0c
Lina Castro <112339197+linakl19@users.noreply.github.com>
2024-11-13 20:17:53 -0800

    Created TodoList component.

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import TodoList from './TodoList'
 
-//List of todo items,each with a unique id and title
-const todoList = [
-  {
-    id:1,
-    title: "Take dogs for a walk."
-  },
-  {
-    id:2,
-    title: "Make dinner."
-  },
-  {
-    id:3,
-    title: "Complete CTD assigment."
-  }
-];
 
 //App component
 function App() {
@@ -26,13 +12,7 @@ function App() {
       <div>
         {/* Level 1 heading */}
         <h1>Todo List</h1>
-
-        {/* Unordered list that renders each item in the todoList array as a list item */}
-        <ul>
-          {todoList.map(function(item){
-            return <li key={item.id}>{item.title}</li>;
-          })}
-        </ul>
+        <TodoList />
       </div>
     </>
   )
