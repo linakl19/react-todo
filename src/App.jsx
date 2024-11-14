956fca5fb0d7eaa4f50d482c7a1bc43a44738670
Lina Castro <112339197+linakl19@users.noreply.github.com>
2024-11-13 20:29:32 -0800

    Created instance of AddTodoForm component.

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import TodoList from './TodoList'
-
+import AddTodoForm from './AddTodoForm'
 
 //App component
 function App() {
@@ -12,6 +12,7 @@ function App() {
       <div>
         {/* Level 1 heading */}
         <h1>Todo List</h1>
+        <AddTodoForm />
         <TodoList />
       </div>
     </>
