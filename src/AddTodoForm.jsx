3eca841df5ef34f366b9257491661cde37b203ac
Lina Castro <112339197+linakl19@users.noreply.github.com>
2024-11-19 23:20:11 -0800

    Add props in the AddTodoForm and invoke props.onAddTodo callback prop.

--- src/AddTodoForm.jsx
+++ src/AddTodoForm.jsx
@@ -1,15 +1,22 @@
 import React from "react";
 
-
-const AddTodoForm = () => {
+// Add props as a parameter in the AddTodoForm function
+const AddTodoForm = (props) => {
     // create a new function called handleAddTodo that takes event as a parameter
+
     const handleAddTodoForm = (event) => {
+
         // prevent the default behavior of the form submit
         event.preventDefault();
+        
         // retrieve the value of the title element from the event target
         console.log(event.target);
         let todoTitle = event.target.title.value;
         console.log(todoTitle);
+
+        // invoke the onAddTodo callback prop and pass todoTitle as an argument
+        props.onAddTodo(todoTitle);
+
         // reset the form so the text input value is cleared
         event.target.reset();
     };
